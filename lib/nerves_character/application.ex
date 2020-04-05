c7b9fe85924851a1ef813fa0d06abe75a7e03f63
Adam Zaninovich <adam.zaninovich@gmail.com>
2020-04-05 15:46:35 -0700

    adds docs and cleans up application and main scene

--- lib/nerves_character/application.ex
+++ lib/nerves_character/application.ex
@@ -1,59 +1,41 @@
 defmodule NervesCharacter.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
   require Logger
 
+  @doc false
   def start(_type, _args) do
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: NervesCharacter.Supervisor]
+    main_viewport_config = Application.get_env(:nerves_character, :viewport)
+    children = [{Scenic, viewports: [main_viewport_config]}]
 
-    children =
-      [
-        # Children for all targets
-        # Starts a worker by calling: NervesCharacter.Worker.start_link(arg)
-        # {NervesCharacter.Worker, arg},
-      ] ++ children(target())
-
-    if target() != :host and should_start_wizard?() do
-      Logger.info("VintageNetWizard starting")
-      VintageNetWizard.run_wizard(on_exit: {__MODULE__, :handle_wizard_exit, []})
-    else
-      Logger.error("wlan0 configured, skipping VintageNetWizard")
-    end
+    start_wifi_wizard_when_needed()
 
     Supervisor.start_link(children, opts)
   end
 
-  def handle_wizard_exit() do
-    Logger.info("VintageNetWizard stopped")
-  end
+  if Application.get_env(:nerves_character, :target) == :host do
+    defp start_wifi_wizard_when_needed() do
+      Logger.debug("Target is host, skipping VintageNetWizard")
+    end
+  else
+    defp start_wifi_wizard_when_needed() do
+      if should_start_wizard?() do
+        Logger.info("VintageNetWizard starting")
+        VintageNetWizard.run_wizard(on_exit: {__MODULE__, :handle_wizard_exit, []})
+      else
+        Logger.debug("wlan0 configured, skipping VintageNetWizard")
+      end
+    end
 
-  def should_start_wizard? do
-    "wlan0" not in VintageNet.configured_interfaces()
-  end
+    @doc false
+    def handle_wizard_exit() do
+      Logger.info("VintageNetWizard stopped")
+    end
 
-  # List all child processes to be supervised
-  def children(:host) do
-    main_viewport_config = Application.get_env(:nerves_character, :viewport)
-
-    [
-      {Scenic, viewports: [main_viewport_config]}
-    ]
-  end
-
-  def children(_target) do
-    main_viewport_config = Application.get_env(:nerves_character, :viewport)
-
-    [
-      {Scenic, viewports: [main_viewport_config]}
-    ]
-  end
-
-  def target() do
-    Application.get_env(:nerves_character, :target)
+    defp should_start_wizard? do
+      "wlan0" not in VintageNet.configured_interfaces()
+    end
   end
 end
